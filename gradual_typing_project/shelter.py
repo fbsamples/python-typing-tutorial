331a3c8814e3cf0886c4a12775751e94f2c17362
Shannon Zhu <szhu@fb.com>
2022-04-18 13:57:02 -0700

    Fix run logic

--- gradual_typing_project/shelter.py
+++ gradual_typing_project/shelter.py
@@ -17,16 +17,16 @@ class Shelter:
         for name in input['dogs']:
             self.dogs.append(Dog(name))
         for name in input['people']:
-            self.people.append(People(name))
+            self.people.append(Person(name))
 
     def run(self):
         # Some random animal interactions
         all_animals = self.cats + self.dogs
-        for _ in random.randint(0, 5):
+        for _ in range(random.randint(0, 5)):
             play(random.choice(all_animals), random.choice(all_animals))
 
         # Some attempted adoptions
         for person in self.people:
-            for _ in random.randint(0, 3):
+            for _ in range(random.randint(0, 3)):
                 adopt(person, random.choice(all_animals))
 
