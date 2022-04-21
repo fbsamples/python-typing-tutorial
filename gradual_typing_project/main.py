575373e8d3e6a5ef359d3d7873bc8045af4b4937
Shannon Zhu <szhu@fb.com>
2022-04-20 18:08:08 -0700

    Fix print example

--- gradual_typing_project/main.py
+++ gradual_typing_project/main.py
@@ -4,10 +4,12 @@ from shelter import Shelter
 
 
 def print_stats(shelter):
+    # Print counts.
     cats = str(len(shelter.cats))
     dogs = str(len(shelter.dogs)) 
     print("There are " + cats + " cats, " + dogs + " dogs.")
 
+    # Print ratio.
     ratio = str(cats / dogs)
     print("Ratio of cats:dogs is " + ratio)
 
@@ -22,7 +24,7 @@ def main():
     # Simulate 8 hours of activity at the shelter
     for _ in range(8):
         stats = print_stats(shelter)
-        print_stats(stats)
+        print(stats)
         shelter.run()
 
 
