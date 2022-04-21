0c1b8855af0f9e04d952b08ccfc897392ef9ee2c
Shannon Zhu <szhu@fb.com>
2022-04-20 22:03:52 -0700

    Simplify example logic

--- gradual_typing_project/interact.py
+++ gradual_typing_project/interact.py
@@ -1,30 +1,26 @@
 # Helper library for creature interactions
-
+ 
 from typing import Any
 
 from creature import Creature, Person, Cat, Dog
 
-def are_enemies(creature_a, creature_b):
-    return creature_a in creature_b.enemies or creature_b in creature_a.enemies 
+def are_enemies(a, b):
+    return a in b.enemies or b in a.enemies 
 
-def are_friends(creature_a, creature_b):
-    if are_enemies(creature_a, creature_b):
-        return False
-    return creature_a in creature_b.friends or creature_b in creature_a.friends 
+def are_friends(a, b):
+    return a in b.friends or b in a.friends 
 
-def play(creature_a, creature_b):
-    if are_friends(creature_a, creature_b):
-        print("{} and {} get along!".format(creature_a.name, creature_b.name))
-    if are_enemies(creature_a, creature_b):
-        print("{} and {} have a fight!".format(creature_a.name, creature_b.name))
+def play(a, b):
+    if are_friends(a, b):
+        print("{} and {} get along!".format(a.name, b.name))
+    if are_enemies(a, b):
+        print("{} and {} have a fight!".format(a.name, b.name))
     else:
-        creature_a.meet(creature_b)
-        creature_b.meet(creature_a)
+        a.meet(b)
+        b.meet(a)
 
 def adopt(person, pet):
     pet.meet(person)
-    if are_friends(person, pet) and all(
-	are_friends(existing_pet, pet) for existing_pet in person.pets
-    ):
+    if are_friends(person, pet):
         person.adopt(pet)
     
