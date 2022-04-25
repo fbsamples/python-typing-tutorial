dd46b1c2ef64ac615d049c42a448a1dc646f4037
Pradeep Kumar Srinivasan <gohanpra@gmail.com>
2022-04-25 08:35:40 -0700

    introduction: add exercises for functions, classes, data structures.

---
+++ introduction_exercises/classes/01-simple-class-solution.py
@@ -0,0 +1,19 @@
+class Talk:
+  """
+  >>> str(Talk("Python Typing Tutorial", 13))
+  '1 PM - Python Typing Tutorial'
+  """
+
+  # Step 1: Annotate the constructor.
+  def __init__(self, title: str, hour: int) -> None:
+      self.title = title
+      self.hour = hour
+
+  # Step 2: Annotate this method.
+  def __str__(self) -> str:
+      am_pm_string = "AM" if self.hour < 12 else "PM"
+      return f"{self.hour % 12} {am_pm_string} - {self.title}"
+
+# Step 3: Identity the bug present in one of these calls.
+Talk("Python Typing Tutorial", "right now")
+Talk("Python Typing Tutorial", 13)
---
+++ introduction_exercises/functions/02-multiple-functions-solution.py
@@ -0,0 +1,20 @@
+# Step 1: Annotate `get_name`.
+# Step 2: Annotate `greet`.
+# Step 3: Identify the bug in `greet`.
+
+def num_vowels(s: str) -> int:
+   result = 0
+   for letter in s:
+       if letter in "aeiouAEIOU":
+           result += 1
+   return result
+
+def get_name() -> str:
+   return "YOUR NAME HERE"
+
+def greet(name: str) -> None:
+   print("Hello " + name + "! Your name contains " + num_vowels(name) + " vowels.")
+
+# Step 4: Is this call necessary? Does Pyre catch the above bug even without
+# this line?
+greet(get_name())
