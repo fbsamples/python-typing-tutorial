9795e56dc56d19cd58a64aa8ec48460fc041475f
Pradeep Kumar Srinivasan <gohanpra@gmail.com>
2022-04-25 07:39:42 -0700

    introduction: move existing exercises to assorted/.

--- introduction_exercises/assorted-exercises/complex.py
+++ introduction_exercises/assorted-exercises/complex.py
@@ -1,7 +1,7 @@
 # Purpose: Get familiar with more datatypes: float and complex,
 # as well as practice with optional
 
-def to_complex(a_component b_component):
+def to_complex(a_component, b_component):
     """ Construct a complex number (a_component + (i * b_component)),
     where a_component and b_component default to 0 if passed in as None.
     """
