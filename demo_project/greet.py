8d422e49db3b7db23de21ef5e3c1e8dbddba0a38
Shannon Zhu <szhu@fb.com>
2022-04-26 14:59:18 -0700

    test pr

--- demo_project/greet.py
+++ demo_project/greet.py
@@ -1,12 +1,11 @@
 
-def greet(subject: str, repeat_count: int) -> None:
+def greet(subject: str) -> None:
     greeting = "Hello " + subject + "!"
-    for _ in range(repeat_count):
-    	print(greeting)
+    print(greeting)
 
 def main() -> None:
-    repeat_count = input("Greet how many times: ")
-    greet("World", repeat_count)
+    repeat_count = int(input("Greet how many times: "))
+    greet("World")
 
 if __name__ == "__main__":
     main()
