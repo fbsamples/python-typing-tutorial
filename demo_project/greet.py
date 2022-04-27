e53f47e564f00085c1fe8a8f2da149b718457219
Shannon Zhu <szhu@fb.com>
2022-04-26 20:59:41 -0700

    Demo: Remove repeat_count param

--- demo_project/greet.py
+++ demo_project/greet.py
@@ -1,12 +1,10 @@
 
-def greet(subject: str, repeat_count: int) -> None:
+def greet(subject: str) -> None:
     greeting = "Hello " + subject + "!"
-    for _ in range(repeat_count):
-    	print(greeting)
+    print(greeting)
 
 def main() -> None:
-    repeat_count = input("Greet how many times: ")
-    greet("World", repeat_count)
+    greet("World")
 
 if __name__ == "__main__":
     main()
